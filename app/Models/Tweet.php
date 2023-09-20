73eb06acb1407988db063329a39077b739bd7d74
Scarrus666 <scarrus@gmail.com>
2023-09-20 13:46:42 +0200

    first commit

---
+++ app/Models/Tweet.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    use HasFactory;
+
+    // IS NOT NECESSARY
+    // LARAVEL AUTOMATICALLY LINKS 'Tweet' with 'tweets'
+    protected $table = 'tweets';
+}
---
+++ resources/views/tweets/index.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- The only way to do great work is to love what you do. - Steve Jobs -->
+    <h1>My Tweets will be shown here</h1>
+</div>
