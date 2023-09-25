b2601c7dd7443adf74ae644b8f248995a8dcaf5c
Scarrus666 <scarrus@gmail.com>
2023-09-25 09:37:10 +0200

    working on the layout

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header class="sticky-header">
+        <div class="container">
+            <div class="header-left">
+                <a href="/">Main Page</a>
+            </div>
+            <div class="header-right">
+                <a href="/tweets">See My Tweets</a>
+                <a href="/create">Create Tweet</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="content">
+        <!-- Your main content goes here -->
+    </div>
+
+    <footer>
+        <div class="container">
+            <div class="footer-left">
+                <!-- Left side text -->
+            </div>
+            <div class="footer-right">
+                <!-- Right side text -->
+            </div>
+        </div>
+    </footer> 
+</body>
+</html>
+
+
