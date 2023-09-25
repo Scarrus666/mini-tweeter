9b7737ae4c75c286eaeaa1b707b0e99ee71adbf3
Scarrus666 <scarrus@gmail.com>
2023-09-25 16:56:51 +0200

    finished today

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tweets - @yield('title')</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div>
+    @yield('header')
+    <header class="sticky-header">
+            <div class="header-left">
+                <a href="/">Main Page</a>
+            </div>
+            <div class="header-right">
+                <a href="/tweets">My Tweets</a>
+                <a href="{{url('create')}}">Create Tweet</a>
+            </div>
+        </div>
+        </header>
+    </div>
+
+    @yield('content')
+    <div class="maincontent">
+    </div>
+    
+    @yield('footer')
+    <footer class="footer">
+            <p class="footer-left">Mini-Twitter built with Laravel</p>
+            <p class="footer-right">© Vorname Nachname</p>
+    </footer>
+    
+</body>
+</html>
+
+
--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    @extends('/components/app')
+    @extends('/components/layout')
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mini-Tweeter von Alex Iordachel</title>
@@ -9,30 +9,34 @@
 </head>
 <body>
 
-    
+    @section('header')
+    @endsection
 
-    <div class="container">
+{{--        OLD CODE 
+            <div class="container">
+            <h1>Welcome to Mini-Tweeter</h1>
+            <a href="{{url('create')}}">Submit Message</a>
+            <a href="{{url('show')}}">View/Delete Messages</a>
+        </div> --}}
 
-        <h1>Welcome to Mini-Tweeter</h1>
-
-        <a href="{{url('create')}}">Submit Message</a>
-        <a href="{{url('show')}}">View/Delete Messages</a>
-
-    </div>
-
-    @foreach ($tweets as $tweet)
+    @section('content')
+        @foreach ($tweets as $tweet)
         <div class="container">
-
+            
             <h3>{{ $tweet->title }}</h3>
             <p>{{ $tweet->text }}</p>
             <p>Updated at {{ $tweet->updated_at }}</p>
-
+            
             <a href="/tweets/{{$tweet->id}}">
                 <button class="viewButton">View complete Message</button>
             </a>
             
         </div>
-    @endforeach
+        @endforeach
+    @endsection
+
+    @section('footer')
+    @endsection
 
 </body>
 </html>
